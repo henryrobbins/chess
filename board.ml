85c83166a36699eb07c2f3c0b26e33571c68bbc5
yk765 <yk765@cornell.edu>
2021-03-24 16:41:11 -0400

    Created init_from_json; edited the square type so that it had the key "pos" that correlated to the position of the piece. TODO: change from_init_json or the initial json file to accomodate for the new square type.

--- board.ml
+++ board.ml
@@ -1,22 +1,24 @@
 open Yojson.Basic.Util
 
-type square = string
+type square = {
+  pos : string}
 
 type direction = N | NE | E | SE | S | SW | W | NW | L
 
 type p = {
   id : string;
   color : string;
-  current_pos : square option
+  positions : square list;
+  number : string;
 }
 
 let ranks = ["1"; "2"; "3"; "4"; "5"; "6"; "7"; "8"]
 let files = ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"]
 
-type b = (string * p option) list
+type b = (square * p option) list
 
 type t = {
-  board : b;
+  (**board : b;*)
   active_pieces : p list;
   captured_pieces : p list;
 }
@@ -39,6 +41,28 @@ let active_pieces t = failwith("Not implemented.")
 
 let captured_pieces t = failwith("Not implemented.")
 
-let init_from_json t = failwith("Not implemented.")
+let init_from_json board =  
+
+let extract_positions (position1 : Yojson.Basic.t) : square =
+  {
+  pos = position1 |> member "pos" |> to_string;
+  }
+in
+let extract_piece (piece1 : Yojson.Basic.t) : p =
+  {
+    id = piece1 |> member "id" |> to_string;
+    color = piece1 |> member "color" |> to_string;
+    positions = piece1 |> member "positions" |> to_list |> List.map extract_positions;
+    number = piece1 |> member "number" |> to_string;
+  }
+in
+let extract_board (board1 : Yojson.Basic.t)  =
+  {
+    active_pieces = board1 |> member "active_pieces" |> to_list |> List.map extract_piece;
+    captured_pieces = board1 |> member "captured_pieces" |> to_list |> List.map extract_piece;
+  }
+in
+extract_board 
+
 
 let print t = failwith("Not implemented.")
