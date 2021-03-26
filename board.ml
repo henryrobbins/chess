b96b2d824d49e25da7b674932d0920890b8ec4c0
Henry Robbins <hwr26@cornell.edu>
2021-03-26 12:40:26 -0400

    Implemented move_piece and wrote iterator_from_sq tests

--- board.ml
+++ board.ml
@@ -21,29 +21,54 @@ type t = {
   captured_pieces : p list;
 }
 
+let active_pieces t = t.active_pieces
+
+let captured_pieces t = t.captured_pieces
+
 let piece_of_square t square = List.assoc square t.board
 
-let square_of_piece t p = failwith("Not implemented.")
+let square_of_piece p = p.current_pos
 
 let id_of_piece p =
   match p with
   | None -> failwith("Piece must be non-None.")
   | Some p -> p.id
 
-let move_piece t p s = failwith("Not implemented")
-
-let start_sq_of_piece p = failwith("Not implemented.")
+let move_piece t piece s' =
+  match piece with
+  | None -> failwith("Requires [p] is a non-None piece option.")
+  | Some p ->
+    let p' = {p with current_pos = Some s'} in
+    let active =
+      match piece_of_square t s' with
+      | None -> (active_pieces t)
+                |> List.filter (fun x -> x <> p)
+                |> List.cons p'
+      | Some cp -> (active_pieces t)
+                  |> List.filter (fun x -> x <> p)
+                  |> List.filter (fun x -> x <> cp)
+                  |> List.cons p'
+                  |> List.cons {cp with current_pos = None} in
+    let captured =
+      match piece_of_square t s' with
+      | None -> captured_pieces t
+      | Some cp -> {cp with current_pos = None} :: captured_pieces t in
+    let board =
+      match square_of_piece p with
+      | None -> failwith "Piece should be active."
+      | Some s -> t.board
+                  |> List.remove_assoc s
+                  |> List.cons (s, None)
+                  |> List.remove_assoc s'
+                  |> List.cons (s', Some p') in
+    {board=board; active_pieces=active; captured_pieces=captured}
 
 let color_of_piece p =
   match p with
-    | None -> failwith("Piece must be non-None.")
-    | Some p -> p.color
+  | None -> failwith("Piece must be non-None.")
+  | Some p -> p.color
 
 let iterator_from_sq square direction = failwith("Not implemented.")
-
-let active_pieces t = t.active_pieces
-
-let captured_pieces t = t.captured_pieces
 
 (** [extract_active_piece j] extracts a list of active pieces from JSON.
     Requires: JSON is in valid format. *)
@@ -73,12 +98,12 @@ let extract_captured_piece j : p list =
 let blank_board : b =
   let rec init rows cols b =
     match rows with
-      | [] -> b
-      | rh :: rt -> begin
-        match cols with
-          | [] -> init rt files b
-          | ch :: ct -> init (rh :: rt) ct (((ch ^ rh),None) :: b)
-      end in
+    | [] -> b
+    | rh :: rt -> begin
+      match cols with
+      | [] -> init rt files b
+      | ch :: ct -> init (rh :: rt) ct (((ch ^ rh),None) :: b)
+    end in
   init ranks files []
 
 let init_from_json j =
@@ -92,11 +117,11 @@ let init_from_json j =
                           |> List.flatten in
   let rec add_pieces b piece_list =
     match piece_list with
-      | [] -> b
-      | h :: t -> let pos = h.current_pos in
-        match pos with
-          | None -> failwith("Active pieces should have a position.")
-          | Some pos -> add_pieces ((pos, (Some h)) :: b) t in
+    | [] -> b
+    | h :: t -> let pos = h.current_pos in
+      match pos with
+      | None -> failwith("Active pieces should have a position.")
+      | Some pos -> add_pieces ((pos, (Some h)) :: b) t in
   let board = add_pieces blank_board active_pieces in
   {
     board = board;
@@ -108,19 +133,19 @@ let init_from_json j =
     If the piece is [None], it is a blank space. *)
 let print_piece p =
   match p with
-    | None -> "  "
-    | Some p ->
-      let c_map = [("White","W"); ("Black", "B")] in
-      let c = List.assoc p.color c_map in
-      c ^ p.id
+  | None -> "  "
+  | Some p ->
+    let c_map = [("White","W"); ("Black", "B")] in
+    let c = List.assoc p.color c_map in
+    c ^ p.id
 
 (* TODO: Print our graveyard pieces somewhere. *)
 let print t : unit =
-  print_string "    a    b    c    d    e    f    g    h\n";
   print_string "  -----------------------------------------\n";
   let row_str i = List.map (fun x -> x ^ i) files
                   |> List.map (piece_of_square t)
                   |> List.map print_piece
                   |> List.fold_left (fun x y -> x ^ " | " ^ y) ""
-  in List.iter (fun r -> print_string (r ^ row_str r ^ " |\n")) ranks;
+  in List.iter (fun r -> print_string (r ^ row_str r ^ " |\n")) (List.rev ranks);
   print_string "  -----------------------------------------\n";
+  print_string "    a    b    c    d    e    f    g    h\n";
