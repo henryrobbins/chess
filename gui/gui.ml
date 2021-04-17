607f16b40e51e23349056323cecc465531e90ab2
Henry Robbins <hwr26@cornell.edu>
2021-04-16 20:11:03 -0400

    Hooked the GUI up to the backend move validation

--- gui/gui.ml
+++ gui/gui.ml
@@ -1,10 +1,48 @@
 open GMain
 open GdkKeysyms
 open Board
+open Command
+open Validation
 
 (* Static parameters for the GUI *)
 let width = 250
 let height = 250
+
+(* TODO: Decide how to remove duplication of this code from main.ml *)
+
+let string_of_string_tup_list tup_lst =
+  let rev_lst = List.rev tup_lst in
+  let rec build_str str lst' =
+    match lst' with
+    | [] -> str
+    | (h, h') :: t ->
+        build_str (str ^ "(" ^ h ^ ", " ^ h' ^ ")" ^ ", ") t
+  in
+  build_str "" rev_lst
+
+let print_pins pin_lst =
+  let rev_lst = List.rev pin_lst in
+  let rec build_str str lst' =
+    match lst' with
+    | [] -> str
+    | h :: t -> build_str (str ^ square_of_piece h ^ ", ") t
+  in
+  build_str "" rev_lst
+
+let print_checkmate_stalemate board =
+  if is_checkmate board then print_string "CHECKMATE \n"
+  else if is_stalemate board then print_string "STALEMATE \n"
+  else ();
+  if is_checkmate board || is_stalemate board then exit 0 else ()
+
+let update_with_move_phrase board sq sq' =
+    let p =
+      match piece_of_square board sq with
+      | None -> failwith "never will happen"
+      | Some p' -> p'
+    in
+    if is_valid_move (sq, sq') board then move_piece board p sq'
+    else board
 
 (* Connect a signal handler, ignoring the resulting signal ID.
    This avoids having to use [|> ignore] everywhere.
@@ -81,16 +119,20 @@ let main () =
             (* update *)
             let p = piece_of_square !board a in
             match p with
-            | None -> print_endline "no piece here"
+            | None -> print_endline "impossible"
             | Some p ->
-              (* update board *)
-              board := move_piece !board p b;
-              (* update buttons *)
-              from_btn#set_label "";
-              let p_id = print_piece (Some p) in
-              to_btn#set_label p_id;
-              print_endline ("move "^p_id^" "^a^" to "^b);
-          );
+              let board' = update_with_move_phrase !board a b in
+              if !board = board' then
+                print_endline "invalid move."
+              else (
+                board := board';
+                from_btn#set_label (print_piece (piece_of_square board' a));
+                to_btn#set_label (print_piece (piece_of_square board' b)));
+          print_endline "==================TESTING==================";
+          print_game_state board';
+          print_checkmate_stalemate board';
+          print_endline "===========================================";
+        );
         set_callbacks (r :: rt) ct in
 
   set_callbacks files ranks;
