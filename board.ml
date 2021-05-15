0428e2d45d18592c2c029b1c2a00ef019850b537
naluconcepcion <lac327@cornell.edu>
2021-05-15 02:39:29 -0400

    implementing puzzles skeleton

--- board.ml
+++ board.ml
@@ -50,6 +50,8 @@ type t = {
   ep_piece : p option;
   half_turns : int;
   full_turns : int;
+
+  puzzle_history: bool list;
 }
 
 (** [rev_map lst] is association list [lst] with keys and values
@@ -141,6 +143,35 @@ let square_of_king t c =
   in
   let king = List.find king_matcher active_pieces in
   square_of_piece king
+
+(** [puzzle_history t] is a list of booleans representing which puzzles
+    the user has correctly answered. *)
+let puzzle_history t = t.puzzle_history
+
+(** [puzzle_streak t] is the number of puzzles the user has answered 
+    correctly in the puzzle state t. *)
+let puzzle_streak t = 
+  let ph = puzzle_history t in 
+  let rec helper p acc = 
+    match p with 
+    | [] -> acc
+    | h :: t -> if h then helper t (acc + 1) else helper t acc in
+  helper ph 0
+
+(** [failed_count t] is the number of failed puzzles (i.e. the number of 
+    "strikes") that the player has in state t. *)
+let failed_count t = (puzzle_history t |> List.length) - puzzle_streak t 
+
+(** [puzzle_move puz p m] is the next puzzle step in puzzle [puz], given that 
+    the user moved piece [p] to square [m]. If [m] was the optimal square to
+    move to, [puz] advances to its next state, if there is one. If [puz] does
+      not have a next state, [puzzle_step] is true. If [m] was not the optimal
+    square, then [puzzle_step] is false. *)
+let rec puzzle_move puz p m = failwith "Unimplemented"
+
+(** [play_puzzles] is the current puzzle state, given that we begin in a
+    puzzle state. *)
+let rec play_puzzles () = failwith "Unimplemented"
 
 let capture_piece t piece =
   let sq = square_of_piece piece in
@@ -504,6 +535,7 @@ let init_from_fen fen =
         ep_piece = get_ep_piece active_pieces color_to_move en_passant;
         half_turns = int_of_string ht;
         full_turns = int_of_string ft;
+        puzzle_history = [];
       }
   | _ -> failwith "impossible"
 
@@ -575,6 +607,15 @@ let export_to_fen t =
   ^ string_of_int t.half_turns
   ^ " "
   ^ string_of_int t.full_turns
+
+
+(** [compare_move cur opt] is a boolean representing whether or not the current
+    player who's turn it is to move made the best possible move. *)
+let compare_move cur opt = failwith "Unimplemented"
+
+(** [init_puzzle p] is the game state initialized from puzzle [p]. Requires:
+    [p] is an FEN string. *)
+let init_puzzle p = init_from_fen p
 
 let init_game () =
   init_from_fen
