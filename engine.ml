29b8ea82f2d9d3184830acb660ff11974bfb67bb
Anders Wikum <aew236@cornell.edu>
2021-05-17 00:42:28 -0400

    best_move now handles checkmates, pawn promotion

--- engine.ml
+++ engine.ml
@@ -1,14 +1,19 @@
+open Board
+
 let parse_engine_response s =
-  print_endline s;
   let strs = String.split_on_char ' ' s in
   match strs with
   | [ "bestmove"; move; "ponder"; next_move ] -> move
+  | [ "bestmove"; move ] -> move
   | _ -> failwith "stockfish search failure"
 
 let extract_engine_move s =
   let sq1 = String.sub s 0 2 in
   let sq2 = String.sub s 2 2 in
-  (sq1, sq2)
+  if String.length s = 5 then
+    let promote_id = piece_id_of_string (String.sub s 4 1) in
+    ((sq1, sq2), Some promote_id)
+  else ((sq1, sq2), None)
 
 let best_move fen =
   let recover_output channel =
--- engine.mli
+++ engine.mli
@@ -1,3 +1,5 @@
+open Board
+
 (* [best_move fen] is the best move returned by stockfish in the board
    state represented by the FEN representation [fen].*)
-val best_move : string -> string * string
+val best_move : string -> (string * string) * piece_type option
