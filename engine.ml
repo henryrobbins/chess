4563ac19c7c2a289aba4a7c3659d870c6d7f7ecc
Anders Wikum <aew236@cornell.edu>
2021-05-21 13:56:21 -0400

    Fixed computer pawn promotion bug

--- engine.ml
+++ engine.ml
@@ -8,10 +8,13 @@ let parse_engine_response s =
   | _ -> failwith "stockfish search failure"
 
 let extract_engine_move s =
+  print_endline s;
   let sq1 = String.sub s 0 2 in
   let sq2 = String.sub s 2 2 in
   if String.length (String.trim s) = 5 then
-    let promote_id = piece_id_of_string (String.sub s 4 1) in
+    let promote_id =
+      String.sub s 4 1 |> String.uppercase_ascii |> piece_id_of_string
+    in
     ((sq1, sq2), Some promote_id)
   else ((sq1, sq2), None)
 
