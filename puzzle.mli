400665bed9177e803cfb806161168f1c0e9cae1a
Henry Robbins <hwr26@cornell.edu>
2021-05-22 16:20:37 -0400

    Auto-format

--- puzzle.mli
+++ puzzle.mli
@@ -13,7 +13,7 @@ type fen = string
 (** [get_puz_current_board puz] is the current board in puzzle state [puz].*)
 val get_puz_current_board : puz -> fen
 
-(** [get_player_moves puz] is the list of FENs of optimal moves in puzzle 
+(** [get_player_moves puz] is the list of FENs of optimal moves in puzzle
     state [puz].*)
 val get_player_moves : puz -> fen list
 
@@ -29,34 +29,34 @@ val get_wrong : puz -> bool
     in rush state [rush].*)
 val get_remaining : rush -> puz list
 
-(** [current_puz rush] is the current puzzle being solved in rush state [rush]. 
+(** [current_puz rush] is the current puzzle being solved in rush state [rush].
     *)
 val current_puz : rush -> puz
 
-(** [solved_rush rush] is the number of puzzles the user has answered correctly 
+(** [solved_rush rush] is the number of puzzles the user has answered correctly
 in rush state [rush]. *)
 val solved_rush : rush -> int
 
-(** [wrong_rush rush] is the number of puzzles the user has answered 
+(** [wrong_rush rush] is the number of puzzles the user has answered
     incorrectly in rush state [rush]. *)
 val wrong_rush : rush -> int
 
-(** [puzzle_move puz fen] is the next puzzle step in puzzle [puz], given that 
+(** [puzzle_move puz fen] is the next puzzle step in puzzle [puz], given that
     the user's move resulted in fen [fen]. *)
 val puzzle_move : puz -> fen -> puz
 
 (** [make_rush puz_list init] is the rush instance created by beginning in
     puzzle [init] and continuing through [puz_list].*)
-    val make_rush : puz list -> puz -> rush
+val make_rush : puz list -> puz -> rush
 
-(** [get_next_puzzle rush] is the next puzzle from the remaining puzzles of 
+(** [get_next_puzzle rush] is the next puzzle from the remaining puzzles of
     [rush]. *)
 val get_next_puzzle : rush -> puz
 
 (** [init_rush] is a randomly generated puzzle list of 20 puzzles. *)
-val init_rush : unit -> puz list 
+val init_rush : unit -> puz list
 
-(** [next_puz_from_rush rush] returns the next puzzle from the remaining puzzles 
+(** [next_puz_from_rush rush] returns the next puzzle from the remaining puzzles
     of rush state [rush].*)
 val next_puz_from_rush : rush -> rush
 
