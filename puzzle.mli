879763ff866abd8e74ae6c09b2338be02529c2e5
Anders Wikum <aew236@cornell.edu>
2021-05-23 23:42:14 -0400

    Updates to docs

--- puzzle.mli
+++ puzzle.mli
@@ -1,7 +1,12 @@
 (** Maintains the state of a puzzle *)
 open Board
 
-type progress = InProgress | Complete | GameOver | Correct | Wrong
+type progress =
+  | InProgress
+  | Complete
+  | GameOver
+  | Correct
+  | Wrong
 
 (** The abstract type of values representing a puzzle. *)
 type puzzle
@@ -12,21 +17,31 @@ type rush
 (** The abstract type of values representing an fen. *)
 type fen = string
 
-(** [current_board rush] is the current board of the puzzle being solved. *)
+(** [current_board rush] is the current board of the puzzle being
+    solved. *)
 val current_board : rush -> Board.t
 
-(** [total_solved rush] is the number of puzzles the user has answered correctly
-    in rush state [rush]. *)
+(** [total_solved rush] is the number of puzzles the user has answered
+    correctly in rush state [rush]. *)
 val total_solved : rush -> int
 
-(** [computer_color rush] is the color of the computer in the current puzzle. *)
+(** [computer_color rush] is the color of the computer in the current
+    puzzle. *)
 val computer_color : rush -> Board.color
 
 (** [total_wrong rush] is the number of puzzles the user has answered
     incorrectly in rush state [rush]. *)
 val total_wrong : rush -> int
 
-(** [update_rush_with_move rush fen] TODO *)
+(** [update_rush_with_move rush fen] updates the internal representation
+    [rush] of the rush depending on the correctness of the given move
+    [fen] and whether the puzzle has ended:
+
+    [update_rush_with_move rush fen] is [InProgress] if the current
+    puzzle is not finished, [Wrong] if the provided move was incorrect
+    but the rush is not over, [Correct] if the provided move was correct
+    but the rush is not over, [GameOver] if the rush is over, and
+    [Complete] if all puzzles are complete. *)
 val update_rush_with_move : rush -> fen -> progress
 
 (** [init_rush] is a randomly generated puzzle list of 20 puzzles. *)
